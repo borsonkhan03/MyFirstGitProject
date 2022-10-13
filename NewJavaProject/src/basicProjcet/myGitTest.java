d70aedc5bdeef358c6679ea038905873a1add537
Khan <borsonkhan@yahoo.com>
2022-10-12 20:10:19 -0400

    this is my new gitText

--- NewJavaProject/src/basicProjcet/myGitTest.java
+++ NewJavaProject/src/basicProjcet/myGitTest.java
@@ -5,6 +5,11 @@ public class myGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This class created after git");
+		
+		System.out.println(" this is king khan");
+		
+		System.out.println("this is my new gitProject");
 	}
+
 
 }
