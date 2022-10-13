24f68848b0f637ee6dfec1b7725afe33e8ea76a4
Khan <borsonkhan@yahoo.com>
2022-10-12 20:06:25 -0400

    second commit

---
+++ NewJavaProject/src/basicProjcet/myGitTest.java
@@ -0,0 +1,10 @@
+package basicProjcet;
+
+public class myGitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This class created after git");
+	}
+
+}
